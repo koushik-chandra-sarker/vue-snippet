b93d26634c2ef80ff48b82f13ed460f7eff5edad
koushik <koushiksk.ks@gmail.com>
2022-05-23 12:02:37 +0600

    vue3 snippet with jwt auth with axios interceptor

--- src/router/index.js
+++ src/router/index.js
@@ -5,14 +5,6 @@ import localStorageService from "@/services/utils/localStorageService";
 import PostDetailsPage from "@/view/pages/PostDetailsPage";
 
 const DashboardPage = () => import ( "@/view/pages/Dashboard/DashboardPage");
-import Post from "@/view/pages/Dashboard/Post";
-import Like from "@/view/pages/Dashboard/Like";
-import Comment from "@/view/pages/Dashboard/Comment";
-import Post1 from "@/view/pages/Dashboard/Post-1";
-import Post11 from "@/view/pages/Dashboard/Post-1.1";
-import Post2 from "@/view/pages/Dashboard/Post-2";
-import Like1 from "@/view/pages/Dashboard/Like-1";
-
 const routes = [
     {
         name: "Home",
@@ -41,63 +33,7 @@ const routes = [
             authenticationRequired: true,
             showNavigation: true
         },
-        children: [
-            {
-                name: "Post",
-                path: '/dashboard/post',
-                component: Post,
-                meta: {
-                    authenticationRequired: true
-                },
-            },
-            {
-                name: "Post-1",
-                path: '/dashboard/post-1',
-                component: Post1,
-                meta: {
-                    authenticationRequired: true
-                }
-            },{
-                name: "Post-1.1",
-                path: '/dashboard/post-1.1',
-                component: Post11,
-                meta: {
-                    authenticationRequired: true
-                }
-            },
-            {
-                name: "Post-2",
-                path: '/dashboard/post-2',
-                component: Post2,
-                meta: {
-                    authenticationRequired: true
-                }
-            },
-            {
-                name: "Like",
-                path: '/dashboard/like',
-                component: Like,
-                meta: {
-                    authenticationRequired: true
-                }
-            },
-            {
-                name: "Like-1",
-                path: '/dashboard/like-1',
-                component: Like1,
-                meta: {
-                    authenticationRequired: true
-                }
-            },
-            {
-                name: "Comment",
-                path: '/dashboard/comment',
-                component: Comment,
-                meta: {
-                    authenticationRequired: true
-                }
-            },
-        ],
+        children: [],
     },
     {
         name: "Post Details",
